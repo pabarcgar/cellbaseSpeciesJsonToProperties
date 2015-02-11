7df091bd9157651f2d96aeb3b50dbaa65167a0ae
Pablo Arce <pablo.arce@bioinfomgp.org>
2015-02-11 10:19:32 +0100

    speciesJsonToNewJson.py script now just ouputs species information; id and scientific name added to species

--- speciesJsonToNewJson.py
+++ speciesJsonToNewJson.py
@@ -12,63 +12,82 @@ reg_url = 'regulation_url'
 database = 'database'
 
 
-def cut_variation_url(variation_url):
-    mysql_substr = 'mysql/'
-    index = variation_url.find(mysql_substr)
-    return variation_url[:index + len(mysql_substr)]
+def get_all_alias():
+    server_properties_file = open('server.properties')
+    server_properties_lines = server_properties_file.readlines()
+    alias_lines = [alias_line[:-1] for alias_line in server_properties_lines if 'ALIAS' in alias_line]
+    server_properties_file.close()
+    return alias_lines
 
 
-def get_ensembl_properties(phylo):
-    ensembl_properties = {'host': phylo[db_host], 'port': phylo[db_port]}
-
-    # sequence url
-    seq_url = list({item['sequence_url'] for item in phylo['items']})
-    ensembl_properties['sequence_url'] = seq_url[0]
-
-    #variation url
-    complete_var_urls = [item['variation_url'] for item in phylo['items'] if 'variation_url' in item]
-    trimmed_var_urls = set(map(cut_variation_url, complete_var_urls))
-    ensembl_properties['variation_url'] = list(trimmed_var_urls)[0]
-
-    return ensembl_properties
+# def cut_variation_url(variation_url):
+#     mysql_substr = 'mysql/'
+#     index = variation_url.find(mysql_substr)
+#     return variation_url[:index + len(mysql_substr)]
 
 
-# def get_utl(phylo):
-#     seq_url = p
-#     url_properties = {'sequence': }
+# def get_ensembl_properties(phylo):
+#     ensembl_properties = {'host': phylo[db_host], 'port': phylo[db_port]}
+#
+#     # sequence url
+#     seq_url = list({item['sequence_url'] for item in phylo['items']})
+#     ensembl_properties['sequence_url'] = seq_url[0]
+#
+#     #variation url
+#     complete_var_urls = [item['variation_url'] for item in phylo['items'] if 'variation_url' in item]
+#     trimmed_var_urls = set(map(cut_variation_url, complete_var_urls))
+#     ensembl_properties['variation_url'] = list(trimmed_var_urls)[0]
+#
+#     return ensembl_properties
 
-def get_species_properties(phylo):
+
+def get_species_id(species_name, all_alias):
+    specie_aliases = [line.split("ALIAS = ")[1] for line in all_alias if species_name in line]
+    if len(specie_aliases) == 1:
+        return specie_aliases[0].split(',')[2]
+    else:
+        print species_name + ' has ' + str(len(specie_aliases)) + ' aliases'
+        return ''
+
+
+def get_species_properties(phylo, all_alias):
+    scientific_name_tag = 'scientificName'
+    ensembl_version_tag = 'database'
     species = []
     for specie in phylo['items']:
         species_name = specie['text']
-        existing_specie = [ sp for sp in species if sp['name'] == species_name ]
+        existing_specie = [ sp for sp in species if sp[scientific_name_tag] == species_name ]
+        if 'core_' in specie['database']:
+            ensembl_version = specie['database'].split('core_')[1]
+        elif specie['text'] == 'Saccharomyces cerevisiae':
+            ensembl_version = '24_77_4'
         if len(existing_specie) == 0:
-            species_dict = {}
-            species_dict['name'] = specie['text']
-            species_dict['assembly'] = [{'name':specie['assembly'], 'database':specie['database']}]
+            species_dict = collections.OrderedDict()
+            species_dict['id'] = get_species_id(species_name, all_alias)
+            species_dict[scientific_name_tag] = species_name
+            species_dict['assembly'] = [{'name':specie['assembly'], ensembl_version_tag:ensembl_version}]
             species.append(species_dict)
         else:
-            print specie
-            existing_specie[0]['assembly'].append({'name':specie['assembly'], 'database':specie['database']})
+            #print specie
+            existing_specie[0]['assembly'].append({'name':specie['assembly'], ensembl_version_tag:ensembl_version})
             #species_dict['database'] = specie['database']
 
     return species
 
 
-def process_phylo(phylo):
+def process_phylo(phylo, all_alias):
     phylo_dict = {}
-    phylo_dict['ensembl_database'] = get_ensembl_properties(phylo)
-    phylo_dict['species'] = get_species_properties(phylo)
+    #phylo_dict['ensembl_database'] = get_ensembl_properties(phylo)
+    phylo_dict['species'] = get_species_properties(phylo, all_alias)
     return phylo_dict
 
 
 def sort_download_properties(download_properties):
-    sorted_download_properties = {'items':[]}
+    sorted_download_properties = collections.OrderedDict()
     ordered_phylos = ['Vertebrates', 'Metazoa', 'Fungi', 'Protist', 'Plants']
     for phylo_name in ordered_phylos:
         phylo = download_properties[phylo_name]
-        phylo['name'] = phylo_name
-        sorted_download_properties['items'].append(phylo)
+        sorted_download_properties[phylo_name] = phylo
     return sorted_download_properties
 
 f1 = open('species.json')
@@ -76,15 +95,16 @@ text = f1.read()
 f1.close()
 spinfo = json.loads(text)
 
+all_alias = get_all_alias()
 
 download_properties = {}
 for phylo in spinfo['items']:
     phylo_name = phylo['text']
-    phylo_dict = process_phylo(phylo)
+    phylo_dict = process_phylo(phylo, all_alias)
     download_properties[phylo_name] = phylo_dict
 
 sorted_download_properties = sort_download_properties(download_properties)
 
-properties = {'download': sorted_download_properties}
+properties = {'species': sorted_download_properties}
 
 print json.dumps(properties)
